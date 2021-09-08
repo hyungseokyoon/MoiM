2b07ad93142dd69667a23e956146d0e179ddca6c
IsaacCho <86340058+minjecho@users.noreply.github.com>
2021-09-08 10:32:27 +0900

    Team 관련 클래스(controller, service, dao) 의존성 연결

--- MoiM/src/main/java/com/finalp/moim/team/model/service/TeamServiceImpl.java
+++ MoiM/src/main/java/com/finalp/moim/team/model/service/TeamServiceImpl.java
@@ -1,5 +1,15 @@
 package com.finalp.moim.team.model.service;
 
-public class TeamServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.finalp.moim.team.model.dao.TeamDao;
+
+@Service("teamService")
+public class TeamServiceImpl implements TeamService {
+	// DI
+	@Autowired
+	private TeamDao teamDao;
+	
+	public TeamServiceImpl() {}
 }
