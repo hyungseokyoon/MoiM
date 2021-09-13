f329d9a8edc9f8f61d9f7007a845120577107aca
IsaacCho <86340058+minjecho@users.noreply.github.com>
2021-09-13 17:53:21 +0900

    Notice List, Detail 작업 완료

--- MoiM/src/main/java/com/finalp/moim/notice/model/service/NoticeServiceImpl.java
+++ MoiM/src/main/java/com/finalp/moim/notice/model/service/NoticeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.finalp.moim.notice.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.finalp.moim.common.Page;
 import com.finalp.moim.notice.dao.NoticeDao;
+import com.finalp.moim.notice.model.vo.Notice;
 
 @Service("noticeService")
 public class NoticeServiceImpl implements NoticeService {
@@ -12,4 +16,19 @@ public class NoticeServiceImpl implements NoticeService {
 	private NoticeDao noticeDao;
 	
 	public NoticeServiceImpl() {}
+
+	@Override
+	public int selectListCount() {
+		return noticeDao.selectListCount();
+	}
+
+	@Override
+	public ArrayList<Notice> selectList(Page page) {
+		return noticeDao.selectList(page);
+	}
+
+	@Override
+	public Notice selectNotice(int notice_no) {
+		return noticeDao.selectNotice(notice_no);
+	}
 }
