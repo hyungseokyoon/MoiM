ea1e9b4ced86635629bd255e710b38b95a25bee0
imsuan <imsuan@localhost>
2021-09-25 15:22:50 +0900

    리뷰 검색 후 페이징, 나의 글보기(마이페이지) 완

--- MoiM/src/main/java/com/finalp/moim/mypage/controller/MyPageController.java
+++ MoiM/src/main/java/com/finalp/moim/mypage/controller/MyPageController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.finalp.moim.board.model.vo.Board;
 import com.finalp.moim.mypage.model.service.MyPageService;
-import com.finalp.moim.mypage.model.vo.MyPage;
-import com.finalp.moim.userinfo.model.service.UserInfoService;
-import com.finalp.moim.userinfo.model.vo.UserInfo;
+import com.finalp.moim.mypage.model.vo.MyBoard;
+import com.finalp.moim.mypage.model.vo.MyPost;
+import com.finalp.moim.review.model.vo.Review;
 
 @Controller
 public class MyPageController {
@@ -22,7 +23,7 @@ public class MyPageController {
 
 	// DI
 	@Autowired
-	private UserInfoService userinfoService;
+	private MyPageService mypageService;
 	
 	// 마이페이지 회원확인
 	@RequestMapping("mypage.do")
@@ -51,9 +52,98 @@ public class MyPageController {
 	}
 	// 내가 쓴 글 보기
 	@RequestMapping("upost.do")
-	public String userPostForward() {
-		return "myPage/userPost";  //내보낼 뷰파일명 리턴
-	}
+	public ModelAndView userPostForward(ModelAndView mv, @RequestParam(name="page", required=false) String page,
+			@RequestParam(name="user_no") int user_no) {
+		
+			int currentPage = 1;
+			if(page != null) {
+				currentPage = Integer.parseInt(page);
+			}
+			
+			
+			
+			int limit = 10;
+			int listCount = mypageService.selectMyBoardCount(user_no);
+			
+			int maxPage = (int)((double)listCount / limit + 0.9);
+			int startPage = (int)((double)currentPage / limit + 0.9);
+			int endPage = startPage + 10 - 1;
+			if(maxPage < endPage) {
+				endPage = maxPage;
+			}
+			
+			int startRow = (currentPage - 1) * limit + 1;
+			int endRow = startRow + limit - 1;
+			MyBoard mb = new MyBoard(startRow, endRow, user_no);
+			
+			ArrayList<Board> blist = mypageService.selectMyBoard(mb);
+			
+			
+				mv.addObject("blist", blist);
+				mv.addObject("listCount", listCount);
+				mv.addObject("maxPage", maxPage);
+				mv.addObject("currentPage", currentPage);
+				mv.addObject("startPage", startPage);
+				mv.addObject("endPage", endPage);
+				mv.addObject("limit", limit);
+				mv.addObject("startRow", startRow);
+				mv.addObject("endRow", endRow);
+				
+				mv.setViewName("myPage/userPost");
+			
+			
+			return mv;
+		}
+		
+	
+	@RequestMapping("upost2.do")
+	public ModelAndView userPostForward2(ModelAndView mv, @RequestParam(name="page", required=false) String page,
+			@RequestParam(name="user_nn") String user_nn) {
+		
+			int currentPage = 1;
+			if(page != null) {
+				currentPage = Integer.parseInt(page);
+			}
+			
+			
+			
+			int limit = 10;
+			int listCount = mypageService.selectMyReviewCount(user_nn);
+			
+			int maxPage = (int)((double)listCount / limit + 0.9);
+			int startPage = (int)((double)currentPage / limit + 0.9);
+			int endPage = startPage + 10 - 1;
+			if(maxPage < endPage) {
+				endPage = maxPage;
+			}
+			
+			int startRow = (currentPage - 1) * limit + 1;
+			int endRow = startRow + limit - 1;
+			MyPost mp = new MyPost(startRow, endRow, user_nn);
+			
+			ArrayList<Review> rlist = mypageService.selectMyReview(mp);
+			
+			
+				mv.addObject("rlist", rlist);
+				mv.addObject("listCount", listCount);
+				mv.addObject("maxPage", maxPage);
+				mv.addObject("currentPage", currentPage);
+				mv.addObject("startPage", startPage);
+				mv.addObject("endPage", endPage);
+				mv.addObject("limit", limit);
+				mv.addObject("startRow", startRow);
+				mv.addObject("endRow", endRow);
+				
+				mv.setViewName("myPage/userPost2");
+			
+			
+			return mv;
+		}
+		
+	
+	
+	
+	
 	// 내 팀 보기
 	@RequestMapping("uteam.do")
 	public String userTeamForward() {
--- MoiM/src/main/java/com/finalp/moim/mypage/model/service/MyPageServiceImpl.java
+++ MoiM/src/main/java/com/finalp/moim/mypage/model/service/MyPageServiceImpl.java
@@ -1,5 +1,39 @@
 package com.finalp.moim.mypage.model.service;
 
-public class MyPageServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalp.moim.board.model.vo.Board;
+import com.finalp.moim.mypage.model.dao.MyPageDao;
+import com.finalp.moim.mypage.model.vo.MyBoard;
+import com.finalp.moim.mypage.model.vo.MyPost;
+import com.finalp.moim.review.model.vo.Review;
+
+@Service("MyPageService")
+public class MyPageServiceImpl implements MyPageService{
+
+		@Autowired
+		MyPageDao mypageDao;
+		
+		@Override
+		public ArrayList<Review> selectMyReview(MyPost mp){
+			return mypageDao.selectMyReview(mp);
+		}
+		
+		@Override
+		public int selectMyReviewCount(String user_nn) {
+			return mypageDao.selectMyReviewCount(user_nn);
+		}
+		
+		@Override
+		public ArrayList<Board> selectMyBoard(MyBoard mb){
+			return mypageDao.selectMyBoard(mb);
+		}
+		
+		@Override
+		public int selectMyBoardCount(int user_no) {
+			return mypageDao.selectMyBoardCount(user_no);
+		}
 }
