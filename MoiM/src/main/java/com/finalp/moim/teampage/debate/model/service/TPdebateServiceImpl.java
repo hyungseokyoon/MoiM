098846f11a53d3745992fb15a9709ee218b8b7ec
noirloup <noirloup@kakao.com>
2021-09-28 16:43:42 +0900

    토론 작업 완료_21.09.28

--- MoiM/src/main/java/com/finalp/moim/teampage/debate/model/service/TPdebateServiceImpl.java
+++ MoiM/src/main/java/com/finalp/moim/teampage/debate/model/service/TPdebateServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.finalp.moim.teampage.debate.model.dao.TPdebateDao;
 import com.finalp.moim.teampage.debate.model.vo.Debate;
+import com.finalp.moim.teampage.debate.model.vo.DebateComment;
 
 
 @Service("tpdebateService")
@@ -19,7 +20,55 @@ public class TPdebateServiceImpl implements TPdebateService{
 	public ArrayList<Debate> selectAll(int team_num) {
 		return tpdebatedao.selectList(team_num);
 	}
-	
-	
+
+	@Override
+	public int insertdebate(Debate debate) {
+		return tpdebatedao.insertdebate(debate);
+	}
+
+	@Override
+	public Debate selectDebate(int debate_num) {
+		return tpdebatedao.selectDebate(debate_num);
+	}
+
+	@Override
+	public ArrayList<DebateComment> selectDebateCommentList(int debate_num) {
+		return tpdebatedao.selectDebateCommentList(debate_num);
+	}
+
+	@Override
+	public int updatedebate(Debate debate) {
+		return tpdebatedao.updateDebate(debate);
+	}
+
+	@Override
+	public int deleteDebateCommentAll(int debate_num) {
+		return tpdebatedao.deleteDebateCommentAll(debate_num);
+	}
+
+	@Override
+	public int deleteDebate(int debate_num) {
+		return tpdebatedao.deleteDebate(debate_num);
+	}
+
+	@Override
+	public int insertDebateComment(DebateComment dcomment) {
+		return tpdebatedao.insertDebateComment(dcomment);
+	}
+
+	@Override
+	public int updateDebateCommentCountUp(int debate_num) {
+		return tpdebatedao.updateDebateCommentCountUp(debate_num);
+	}
+
+	@Override
+	public int deleteDebateComment(int debate_comment_num) {
+		return tpdebatedao.deleteDebateComment(debate_comment_num);
+	}
+
+	@Override
+	public int updateDebateCommentCountDown(int debate_num) {
+		return tpdebatedao.updateDebateCommentCountDown(debate_num);
+	}
 	
 }
